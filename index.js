1f933db125e5526cce5b2c552d677dcfae350b91
Revath S Kumar <rsk@revathskumar.com>
2018-06-02 14:07:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Telegram = require('telegram-node-bot')
+
+const TelegramBaseInlineQueryController = Telegram.TelegramBaseInlineQueryController
+const tg = new Telegram.Telegram(process.env.TELEGRAM_BOT_TOKEN, {
+    workers: 1,
+    webhook: {
+        url: process.env.WEBHOOK_URL,
+        port: process.env.WEBHOOK_PORT || 3000,
+        host: 'localhost'
+    }
+})
+
+class DuckController extends TelegramBaseInlineQueryController {
+    handle($) {
+        const query = $._inlineQuery.query;
+        let results = [];
+        if (query) {
+            results = [{
+                id: Math.random().toString(36).substring(7),
+                type:'article',
+                message_text: `https://duckduckgo.com/?q=${encodeURIComponent(query)}`,
+                title: 'Duck It',
+                description: `${query}`
+            }];
+        }
+        $.answer(results, {}, function(result) {
+        });
+    }
+}
+
+tg.router
+    .inlineQuery(new DuckController())
